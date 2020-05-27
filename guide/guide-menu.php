f55927e1cdaf4e51f1406b873e3302ed1f5d211e
MagNad <margot.nadot@gmail.com>
2020-05-27 10:16:32 +0200

    init includes

---
+++ guide/guide-menu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Mixin blocs - Guide UI</title>
+    <link href="https://fonts.googleapis.com/css?family=Bebas+Neue|Kalam:700|Source+Sans+Pro&display=swap"
+        rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/c97506cdaa.js" crossorigin="anonymous"></script>
+    <link href="../styles/global.css" rel="stylesheet">
+    <link href="style-guide.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="UGwrap">
+        <nav class="UGnav">Menu</nav>
