9e329ceb381bbe2f294d1f448b82a252744ae22e
MagNad <margot.nadot@gmail.com>
2020-05-31 15:36:48 +0200

    clean home maquette + fix tuile-image responsive

---
+++ maquettes/maquettes-footer.php
@@ -0,0 +1,121 @@
+    <footer class="footer-site__wrap">
+        <section class="fw-section bottom-bar_1 bg-d-red_light bg-trame_champs_dark">
+            <div class="chimney fluid">
+                <div class="section__container">
+                    <div class="columns">
+                        <div class="column column_1-6">
+                            <h6 class="footer-site__title">L'association<br />Terre de Liens</h6>
+                            <ul class="list-simple_double-arrow footer-site__content">
+                                <li><a href="#">Qui sommes-nous ?</a></li>
+                                <li><a href="#">Notre mission</a></li>
+                                <li><a href="#">Nos actions</a></li>
+                                <li><a href="#">Rapports et chiffres-clés</a></li>
+                            </ul>
+                        </div>
+                        <div class="column column_1-6">
+                            <h6 class="footer-site__title">Tous nos sites</h6>
+                            <ul class="list-simple_double-arrow footer-site__content">
+                                <li><a href="#">Terre de Liens </a></li>
+                                <li><a href="#">Centre de Ressources</a></li>
+                                <li><a href="#">Objectif Terre </a></li>
+                                <li><a href="#">Photothèque </a></li>
+                                <li><a href="#">Parcel</a></li>
+                            </ul>
+                        </div>
+                        <div class="column column_1-6">
+                            <h6 class="footer-site__title">Nous rendre visite</h6>
+                            <div class="footer-site__content">
+                                <p>Association Terres de Liens<br />1, rue Joseph Richard<br />26400 CREST</p>
+                            </div>
+                        </div>
+                        <div class="column column_1-6">
+                            <h6 class="footer-site__title">Votre agence territoriale</h6>
+                            <form class="form small tight text_white">
+                                <div class="form-line list nowrap">
+                                    <li class="form-line__item_field">
+                                        <label class="form-label form-label_top" for="type-select">Votre région</label>
+                                        <div class="form-field form-select">
+                                            <button class="form-select__arrow"><i class="fas fa-chevron-down"
+                                                    aria-hidden="true"></i></button>
+                                            <input class="form-select__prompt" type="text" autocomplete="off"
+                                                placeholder="Sélectionner une région...">
+                                            <div class="form-select__drop">
+                                                <ul class="form-select__option__list">
+                                                    <li class="form-select__option__item">Type 1</li>
+                                                    <li class="form-select__option__item">Type 2</li>
+                                                    <li class="form-select__option__item">Type 3</li>
+                                                    <li class="form-select__option__item">Type 4</li>
+                                                    <li class="form-select__option__item">Type 5</li>
+                                                    <li class="form-select__option__item">Type 2</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="form-line__item form-line__item_button">
+                                        <button class="form-button button basic primary">
+                                            <span class="button__icon">
+                                                <i class="fas fa-search" aria-hidden="true"></i>
+                                            </span>
+                                        </button>
+                                    </li>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="column column_1-6">
+                            <h6 class="footer-site__title">Suivez-nous</h6>
+                            <ul class="menu-buttons list">
+                                <li>
+                                    <a href="#" class="button round small secundary">
+                                        <span class="button__icon"><i class="fab fa-facebook-f"></i></span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="button round small secundary">
+                                        <span class="button__icon"><i class="fab fa-twitter"></i></span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="button round small secundary">
+                                        <span class="button__icon"><i class="fab fa-instagram"></i></span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="button round small secundary">
+                                        <span class="button__icon"><i class="fab fa-linkedin-in"></i></span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="column column_1-6">
+                            <h6 class="footer-site__title">Newsletter</h6>
+                            <div class="footer-site__content">
+                                <p>Suivez notre mouvement en vous abonnant.</p>
+                                <p class="button__wrap">
+                                    <a href="#" class="button basic small primary">
+                                        <span class="button__label">S'abonner</span>
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="fw-section bottom-bar_2">
+            <ul class="menu menu-intersites">
+                <li class="menu__item">
+                    <a href="#" class="menu__link">Mentions légales</a>
+                </li>
+                <li class="menu__item">
+                    <a href="#" class="menu__link">Politique de cookies</a>
+                </li>
+                <li class="menu__item menu__item_separator">
+                    <a href="#" class="menu__link">Conditions d'utilisation</a>
+                </li>
+                <li class="menu__item">
+                    <span class="menu__link menu__link_no-click">©2020 - Terre de Liens | All right reserved</span>
+                </li>
+            </ul>
+        </section>
+    </footer>
+</body>
